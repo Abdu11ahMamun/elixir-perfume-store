e821a56a8a0e9eaaee468c8657e436ceca798ff7
abdu11ah-mamun <cs.abdullah.mamun@gmail.com>
2026-06-30 14:39:16 +0600

    new structure!

---
+++ service/elixir-service/src/main/java/com/elixir/elixir_service/ElixirServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elixir.elixir_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElixirServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElixirServiceApplication.class, args);
+	}
+
+}
---
+++ service/elixir-service/src/test/java/com/elixir/elixir_service/ElixirServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixir.elixir_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
